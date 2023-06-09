f7198b7765d53e317853e64bff2707b078bfbfc5
Peter Zijlstra <peterz@infradead.org>
2023-06-09 21:27:07 +0200

    perf: Simplify perf_event_modify_attr()
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -3175,7 +3175,7 @@ static int perf_event_modify_attr(struct perf_event *event,
 
 	WARN_ON_ONCE(event->ctx->parent_ctx);
 
-	mutex_lock(&event->child_mutex);
+	guard(mutex)(&event->child_mutex);
 	/*
 	 * Event-type-independent attributes must be copied before event-type
 	 * modification, which will validate that final attributes match the
@@ -3184,16 +3184,16 @@ static int perf_event_modify_attr(struct perf_event *event,
 	perf_event_modify_copy_attr(&event->attr, attr);
 	err = func(event, attr);
 	if (err)
-		goto out;
+		return err;
+
 	list_for_each_entry(child, &event->child_list, child_list) {
 		perf_event_modify_copy_attr(&child->attr, attr);
 		err = func(child, attr);
 		if (err)
-			goto out;
+			return err;
 	}
-out:
-	mutex_unlock(&event->child_mutex);
-	return err;
+
+	return 0;
 }
 
 static void __pmu_ctx_sched_out(struct perf_event_pmu_context *pmu_ctx,
