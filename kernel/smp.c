529f3b87915a4930e57781228bfafd4b6d7fbdeb
Peter Zijlstra <peterz@infradead.org>
2023-08-03 10:43:22 +0200

    smp: Mark nr_cpu_ids __ro_after_init
    The variable nr_cpu_ids contains the number of bits set in
    cpu_possible_mask and is thus constant after init.
    
    Mark the variable __ro_after_init to both improve performance and
    enhance security, since the variable is exported. A module writing to
    it would be 'unfortunate'.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>

--- kernel/smp.c
+++ kernel/smp.c
@@ -957,7 +957,7 @@ early_param("maxcpus", maxcpus);
 
 #if (NR_CPUS > 1) && !defined(CONFIG_FORCE_NR_CPUS)
 /* Setup number of possible processor ids */
-unsigned int nr_cpu_ids __read_mostly = NR_CPUS;
+unsigned int nr_cpu_ids __ro_after_init = NR_CPUS;
 EXPORT_SYMBOL(nr_cpu_ids);
 #endif
 
