05c905671d2e128f1046ddbd78d784ddef9dafae
Juergen Gross <jgross@suse.com>
2023-10-30 15:25:05 +0100

    x86/alternative: add indirect call patching
    In order to prepare replacing of paravirt patching with alternative
    patching, add the capability to replace an indirect call with a direct
    one to alternative patching.
    
    This is done via a new flag ALT_FLAG_CALL as the target of the call
    instruction needs to be evaluated using the value of the location
    addressed by the indirect call. For convenience add a macro for a
    default call instruction. In case it is being used without the new
    flag being set, it will result in a BUG() when being executed. As in
    most cases the feature used will be X86_FEATURE_ALWAYS add another
    macro ALT_CALL_ALWAYS usable for the flags parameter of the ALTERNATIVE
    macros.
    
    For a complete replacement handle the special cases of calling a nop
    function and an indirect call of NULL the same way as paravirt does.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20231030142508.1407-3-jgross@suse.com

--- arch/x86/kernel/alternative.c
+++ arch/x86/kernel/alternative.c
@@ -396,6 +396,45 @@ noinstr void BUG_func(void)
 EXPORT_SYMBOL_GPL(BUG_func);
 
 /*
+ * Rewrite the "call BUG_func" replacement to point to the target of the
+ * indirect pv_ops call "call *disp(%ip)".
+ */
+static int alt_replace_call(u8 *instr, u8 *insn_buff, struct alt_instr *a)
+{
+	void *target, *bug = &BUG_func;
+
+	if (a->replacementlen != 5 || insn_buff[0] != CALL_INSN_OPCODE) {
+		pr_err("Alternative: ALT_FLAG_CALL set for a non-call replacement instruction\n");
+		pr_err("  Ignoring the flag for the instruction at %pS (%px)\n", instr, instr);
+		return 5;
+	}
+
+	if (a->instrlen != 6 || instr[0] != 0xff || instr[1] != 0x15) {
+		pr_err("Alternative: ALT_FLAG_CALL set for unrecognized indirect call\n");
+		pr_err("  Not replacing the instruction at %pS (%px)\n", instr, instr);
+		return -1;
+	}
+
+#ifdef CONFIG_X86_64
+	/* ff 15 00 00 00 00   call   *0x0(%rip) */
+	target = *(void **)(instr + a->instrlen + *(s32 *)(instr + 2));
+#else
+	/* ff 15 00 00 00 00   call   *0x0 */
+	target = *(void **)(*(s32 *)(instr + 2));
+#endif
+	if (!target)
+		target = bug;
+
+	/* (BUG_func - .) + (target - BUG_func) := target - . */
+	*(s32 *)(insn_buff + 1) += target - bug;
+
+	if (target == &nop_func)
+		return 0;
+
+	return 5;
+}
+
+/*
  * Replace instructions with better alternatives for this CPU type. This runs
  * before SMP is initialized to avoid SMP problems with self modifying code.
  * This implies that asymmetric systems where APs have less capabilities than
@@ -461,6 +500,12 @@ void __init_or_module noinline apply_alternatives(struct alt_instr *start,
 
 		memcpy(insn_buff, replacement, a->replacementlen);
 		insn_buff_sz = a->replacementlen;
+
+		if (a->flags & ALT_FLAG_CALL) {
+			insn_buff_sz = alt_replace_call(instr, insn_buff, a);
+			if (insn_buff_sz < 0)
+				continue;
+		}
 
 		for (; insn_buff_sz < a->instrlen; insn_buff_sz++)
 			insn_buff[insn_buff_sz] = 0x90;
