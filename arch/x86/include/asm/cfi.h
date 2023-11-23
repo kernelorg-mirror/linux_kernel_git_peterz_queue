442c39013ba6a089a8ac592b1456fa15bfed8fe7
Peter Zijlstra <peterz@infradead.org>
2023-11-23 22:22:54 +0100

    x86: BHI stubs
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>

--- arch/x86/include/asm/cfi.h
+++ arch/x86/include/asm/cfi.h
@@ -102,6 +102,10 @@ enum cfi_mode {
 extern enum cfi_mode cfi_mode;
 extern bool cfi_warn;
 
+typedef u8 bhi_thunk[32];
+extern bhi_thunk __bhi_args[];
+extern bhi_thunk __bhi_args_end[];
+
 struct pt_regs;
 
 #ifdef CONFIG_CFI_CLANG
