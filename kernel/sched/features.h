e579aa0fdf6a866a1f2d8e6ff879ae267b3a4949
Peter Zijlstra <peterz@infradead.org>
2025-03-24 11:44:26 +0100

    sched: Default enable HRTICK
    ... for generic entry architectures. This decouples preemption from
    CONFIG_HZ, leaving only the periodic load-balancer and various
    accounting things relying on the tick.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -63,8 +63,13 @@ SCHED_FEAT(DELAY_ZERO, true)
  */
 SCHED_FEAT(WAKEUP_PREEMPTION, true)
 
+#ifdef CONFIG_GENERIC_ENTRY
+SCHED_FEAT(HRTICK, true)
+SCHED_FEAT(HRTICK_DL, true)
+#else
 SCHED_FEAT(HRTICK, false)
 SCHED_FEAT(HRTICK_DL, false)
+#endif
 
 /*
  * Decrement CPU capacity based on time not spent running tasks
