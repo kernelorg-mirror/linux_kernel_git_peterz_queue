4f8511e89a8ffc2be1f53a239fa9ca826cc497b3
Shrikanth Hegde <sshegde@linux.ibm.com>
2026-09-09 19:26:15 +0530

    virt/steal_governor: Add control knobs for handling steal values
    These are the knobs to control the steal_governor.
    
    interval_ms:
    How often steal governor checks for steal time.
    (Default: 1000 i.e 1 second)
    This controls how fast steal governor driver reacts to changes to
    the contention of physical CPUs.
    Can be set between 100 to 100000. i.e. 100ms to 100seconds.
    100ms is kept as minimum to ensure few meaningful steal values
    accumulate even with HZ=100.
    
    low_threshold:
    lower threshold value in percentage * 100.
    (Default: 200, i.e 2% steal is considered as low threshold)
    This determines what values should be considered as nil/no steal values.
    When steal governor see steal ratio is below or equal to this value, it
    will increase the preferred CPUs by 1 core. Having value as zero
    might cause oscillations
    
    high_threshold:
    higher threshold value in percentage * 100
    (Default: 500, i.e 5% steal is considered as high threshold)
    This determines what values should be considered as high steal values.
    When steal governor sees steal ratio is higher than this value, it will
    reduce the preferred CPUs by 1 core.
    
    module_param_cb methods are used to do the validation checks.
    This helps to ensure one configures sane values.
    Since low and high are dependent, that check is done at module init.
    
    Notes:
    - Parameters values can't be changed at runtime. One has to unload
      the module and change it. Hence recommended to build it as module.
    - Default values may not work well for all configurations. Tune it
      according to the system under test.
    
    Documentation is available at: Documentation/driver-api/steal-governor.rst
    
    Suggested-by: Yury Norov <yury.norov@gmail.com>
    Signed-off-by: Shrikanth Hegde <sshegde@linux.ibm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://patch.msgid.link/20260909135617.871006-12-sshegde@linux.ibm.com

--- drivers/virt/steal_governor.c
+++ drivers/virt/steal_governor.c
@@ -40,7 +40,11 @@ struct steal_governor {
 	struct delayed_work	work;
 };
 
-static struct steal_governor sg_ctx;
+static struct steal_governor sg_ctx = {
+	.interval_ms	=	1000,	/* 1 second */
+	.high_threshold =	500,	/* 5% */
+	.low_threshold	=	200,	/* 2% */
+};
 
 static void restore_preferred_to_active(void)
 {
@@ -51,6 +55,62 @@ static void restore_preferred_to_active(void)
 		set_cpu_preferred(cpu, true);
 }
 
+static int param_set_interval_ms(const char *val, const struct kernel_param *kp)
+{
+	unsigned int interval;
+	int ret;
+
+	ret = kstrtouint(val, 0, &interval);
+	if (ret)
+		return ret;
+
+	if (interval < 100 || interval > 100000) {
+		pr_err("interval_ms must be between 100 and 100000\n");
+		return -EINVAL;
+	}
+
+	return param_set_uint(val, kp);
+}
+
+static const struct kernel_param_ops interval_ms_ops = {
+	.set = param_set_interval_ms,
+	.get = param_get_uint,
+};
+
+module_param_cb(interval_ms, &interval_ms_ops, &sg_ctx.interval_ms, 0444);
+MODULE_PARM_DESC(interval_ms,
+		 "Sampling frequency in milliseconds. default: 1000");
+
+static int param_set_high_threshold(const char *val, const struct kernel_param *kp)
+{
+	unsigned int threshold;
+	int ret;
+
+	ret = kstrtouint(val, 0, &threshold);
+	if (ret)
+		return ret;
+
+	if (threshold >= 100 * 100) {
+		pr_err("high_threshold (%u) can't be more than 99.99%%\n", threshold);
+		return -EINVAL;
+	}
+
+	return param_set_uint(val, kp);
+}
+
+static const struct kernel_param_ops high_threshold_ops = {
+	.set = param_set_high_threshold,
+	.get = param_get_uint,
+};
+
+module_param_cb(high_threshold, &high_threshold_ops, &sg_ctx.high_threshold, 0444);
+MODULE_PARM_DESC(high_threshold,
+		 "High steal threshold. default: 500 i.e 5%. Must be > low_threshold");
+
+module_param_named(low_threshold, sg_ctx.low_threshold, uint, 0444);
+MODULE_PARM_DESC(low_threshold,
+		 "Low steal threshold. default: 200 i.e 2%. Must be < high_threshold");
+
 static int __init steal_governor_init(void)
 {
 #ifdef CONFIG_XEN
@@ -60,7 +120,16 @@ static int __init steal_governor_init(void)
 	}
 #endif
 
-	pr_info("enabled\n");
+	if (sg_ctx.low_threshold >= sg_ctx.high_threshold) {
+		pr_err("low_threshold (%u) must be less than high_threshold (%u)\n",
+		       sg_ctx.low_threshold, sg_ctx.high_threshold);
+		return -EINVAL;
+	}
+
+	sg_ctx.delay = msecs_to_jiffies(sg_ctx.interval_ms);
+	pr_info("enabled. interval: %ums, high_threshold: %u, low_threshold: %u\n",
+		sg_ctx.interval_ms, sg_ctx.high_threshold, sg_ctx.low_threshold);
+
 	return 0;
 }
 
